414004e1a9078205193e3ca7458127eaa2bc7cb3
MarkusMCPEGamer <markus.aa84@gmail.com>
2020-03-22 22:42:38 +0100

    Updated help

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
 :white_check_mark: [--ping]
 :white_check_mark: [--register]: erstellt dir einen Account, für dein Geld (einmalig!).
 :x: [--daily]: gibt dir deine dir täglich zustehenden Münzen
-:x: [--play]: startet das Spiel
+:white_check_mark: [--play]: startet das Spiel
 :white_check_mark: [--coins]: zeigt dir deinen aktuellen Kontostand an.
 :white_check_mark: [--debug] aktiviert debug Nachichten.
 :white_check_mark: [--clear + {zahl 1-99}]: Löscht eine bestimmte Anzahl an Nachichten`)
