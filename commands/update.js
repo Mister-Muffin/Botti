af0fb5077a567f9445e823a53f1df240fb637739
Mister-Muffin <markus.aa84@gmail.com>
2021-03-24 17:58:12 +0100

    u

--- commands/update.js
+++ commands/update.js
@@ -4,7 +4,13 @@ const { execSync, spawn } = require("child_process");
 module.exports = {
     name: "update",
     description: "Update this Bot",
-    options: [],
+    options: [
+        {
+            "name": "npm i",
+            "description": "Run npm i",
+            "type": 5,
+            "required": false
+        },],
     run: async (client, interaction, args) => {
 
         await client.channels.fetch(interaction.channel_id).then(async channel => {
@@ -28,19 +34,21 @@ module.exports = {
                 console.log(`stdout: ${stdout}`);
             });
 
-            await channel.send(`:information_source: Updating dependencies, this may take a while!`);
+            if (args && args.find(arg => arg.name.toLowerCase() == "npm i").value) {
+                await channel.send(`:information_source: Updating dependencies, this may take a while!`);
 
-            execSync("npm i", (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-            });
+                execSync("npm i", (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });
+            }
 
             spawn(process.argv.shift(), process.argv, {
                 cwd: process.cwd(),
