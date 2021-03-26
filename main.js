fc0e265fa0b42aaa5d07542e39e5b858952f8ffd
Mister-Muffin <markus.aa84@gmail.com>
2021-03-26 18:15:48 +0100

    fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.ServiceAccountKey);
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY);
 const path = require('path');
 const pathString = `${path.resolve(__dirname, '.')}/data/gold.json`;
 const goldJson = require(pathString);
