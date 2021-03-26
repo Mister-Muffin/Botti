537335df2b48fa5506c7a774a8f9e0027f983d37
Mister-Muffin <markus.aa84@gmail.com>
2021-03-26 18:43:10 +0100

    changed path

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const admin = require('firebase-admin');
 const serviceAccount = process.env.SERVICE_ACCOUNT_KEY;
-const pathString = `./data/gold.json`;
+const pathString = `${__dirname}/data/gold.json`;
 const goldJson = require(pathString);
 const { readdirSync } = require("fs");
 admin.initializeApp({
