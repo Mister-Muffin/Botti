cb38b6007f2d8015389723f2d9c3e544d8d87a46
Mister-Muffin <markus.aa84@gmail.com>
2021-03-28 20:48:35 +0200

    update schaufel.js

--- commands/schaufel.js
+++ commands/schaufel.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const appDir = path.dirname(require.main.filename);
 
-let schaufeln = 0
 module.exports = {
   name: "schaufel",
   description: "Haue dir oder jemand anderen eine Schaufel an dem Kopf.",
@@ -16,20 +15,20 @@ module.exports = {
     const collectionName = "schaufeln";
 
     try {
-      const authorId = interaction.member.user.id;
       const { bottiDB } = require(`${appDir}/main.js`);
+      const user = args ? args.find(arg => arg.name.toLowerCase() == "name").value : interaction.member.user.id;
 
-      const result = await bottiDB.collection(collectionName).findOne({ id: authorId })
+      const result = await bottiDB.collection(collectionName).findOne({ id: user })
       let schaufeln = result ? result.value : 0;
+
 
       let myobj = { $set: { value: schaufeln + 1, name: interaction.member.user.username } };
 
-      await bottiDB.collection(collectionName).updateOne({ id: authorId }, myobj, { upsert: true })
+      await bottiDB.collection(collectionName).updateOne({ id: user }, myobj, { upsert: true })
 
       schaufeln++;
 
       if (args != null) {
-        const user = args.find(arg => arg.name.toLowerCase() == "name").value
         client.api.interactions(interaction.id, interaction.token).callback.post({
           data: {
             type: 4,
