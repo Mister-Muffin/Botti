8d1be458ca733bd7240dfcbcb84061245799b78f
Mister-Muffin <markus.aa84@gmail.com>
2022-10-15 20:50:52 +0200

    improvements

--- handler/command.js
+++ handler/command.js
@@ -12,22 +12,27 @@ module.exports = (client) => {
     // Read every commands subfolder
     readdirSync(`${appDir}/commands/`).forEach(dir => {
         // Filter so we only have .js command files
-        const commands = readdirSync(`${appDir}/commands/`).filter(file => file.endsWith(".js")).filter(file => file.startsWith("ping"));
+        const commands = readdirSync(`${appDir}/commands/`).filter(file => file.endsWith(".js"));
 
         // Loop over the commands, and add all of them to a collection
         // If there's no name found, prevent it from returning an error,
         // By using a cross in the table we made.
         for (let file of commands) {
             let command = require(`${appDir}/commands/${file}`);
+            try {
+                command.data;
 
-            client.commands.set(command.data.name, command);
+                client.commands.set(command.data.name, command);
 
-            if (command.data.name) {
-                client.commands.set(command.name, command);
-                
-                table.addRow(file, "✅");
-            } else {
-                table.addRow(file, "❌  -> missing a help.name, or help.name is not a string.");
+                if (command.data.name) {
+                    client.commands.set(command.name, command);
+
+                    table.addRow(file, "✅");
+                } else {
+                    table.addRow(file, "❌  -> missing a help.name, or help.name is not a string.");
+                }
+            } catch (e) {
+                console.warn("Command skipped. Not implemented");
             }
 
         }
--- handler/registerCommand.js
+++ handler/registerCommand.js
@@ -1,16 +1,32 @@
-const { REST, SlashCommandBuilder, Routes } = require("discord.js");
+const { REST, Routes } = require("discord.js");
 const { token } = require("../config.json");
+const {readdirSync} = require("fs");
+const path = require("path");
+
+const appDir = path.dirname(require.main.filename).replace("handler", "");
 
 const clientId = "493003865537511436";
 const guildId = "492426074396033035";
 
-const commands = [
-    require("../commands/ping.js").data.toJSON(),
-];
+
+const commands = readdirSync(`${appDir}/commands/`).filter(file => file.endsWith(".js"));
+const commandList = [];
+
+for (let file of commands) {
+    try {
+        let command = require(`${appDir}/commands/${file}`).data.toJSON();
+
+        commandList.push(command);
+    } catch (e) {
+        console.warn("Command skipped. Not implemented");
+    }
+
+}
+
 //
 
 const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandList })
     .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
     .catch(console.error);
